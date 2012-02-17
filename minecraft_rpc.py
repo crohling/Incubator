67dda24afccacf5373d43e931f6651adf8588fd9
Christian <crohling@smtp.antilus.com>
2012-02-17 16:02:08 -0500

    timely commit

--- minecraft_rpc.py
+++ minecraft_rpc.py
@@ -2,54 +2,26 @@ import xmlrpclib, sys
 
 class minecraft_server():
 
-    def __init__(self, username, password, hostname, port):
+    def __init__(self, username, password, hostname, port, proc_name='minecraft'):
+        self.proc_name=proc_name
         self.server = xmlrpclib.Server('http://'+username+':'+password+'@'+hostname+':'+port)
-
     def send_command(self, command):
-        self.server.supervisor.sendProcessStdin('minecraft',command + '\n')
-        return self.server.supervisor.tailProcessStderrLog('minecraft', -1, 256)[0].split('\n')[-2]
+        self.server.supervisor.sendProcessStdin(self.proc_name,command + '\n')
+        return self.server.supervisor.tailProcessStderrLog(self.proc_name, -1, 256)[0].split('\n')[-2]
     
     def give_item(self, player, item_number, number_per_stack, number_of_stacks):
         for i in range(number_of_stacks):
             self.send_command('give ' + player + ' ' + str(item_number) + ' ' + str(number_per_stack))
     
-    def respawn(server, player):
-        self.send_command('give '+player+' 276 64')
-        self.send_command('give '+player+' 277 64')
-        self.send_command('give '+player+' 278 64')
-        self.send_command('give '+player+' 279 64')
-        self.send_command('give '+player+' 310 64')
-        self.send_command('give '+player+' 311 64')
-        self.send_command('give '+player+' 312 64')
-        self.send_command('give '+player+' 313 64')
+    def respawn(self, player):
+        self.send_command('give '+player+' 276')
+        self.send_command('give '+player+' 277')
+        self.send_command('give '+player+' 278')
+        self.send_command('give '+player+' 279')
+        self.send_command('give '+player+' 310')
+        self.send_command('give '+player+' 311')
+        self.send_command('give '+player+' 312')
+        self.send_command('give '+player+' 313')
         self.send_command('give '+player+' 322 64')
-        self.send_command('give '+player+' 326 64')
-        self.send_command('give '+player+' 327 64')
-    
-
-if __name__=="__main__":
-"""
-	Dinky CLI
-"""
-    print sys.argv
-    server = xmlrpclib.Server('http://'+sys.argv[1]+':'+sys.argv[2]+'@'+sys.argv[3]+':'+sys.argv[4])
-    num = 1
-    while True:
-        try:
-            sys.stdout.write('In ['+str(num)+']: ')
-            command_split = sys.stdin.readline().split(' ')
-            if 'quit' in command_split[0]:
-                print '\tQuitting'
-                break
-            try:
-                command = ' '.join(command_split[:-1])
-                times = int(command_split[-1:][0])
-                for i in range(times):
-                    print '\tOut['+str(num)+']: ' + str(send_command(server, command))
-            except Exception as excep:
-                print excep
-            num += 1
-
-        except KeyboardInterrupt:
-            print '\tQuitting'
-            break
+        self.send_command('give '+player+' 326')
+        self.send_command('give '+player+' 327')
